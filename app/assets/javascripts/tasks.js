da0274a122f205977f71f7bc102abf6e695ef124
Fil <runfilrun@gmail.com>
2019-04-25 18:59:32 +0000

    Updates script to strikeout completed items.

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -5,25 +5,32 @@ $(function() {
   // <li> tags
   function taskHtml(task) {
     var checkedStatus = task.done ? "checked" : "";
-    var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
       " data-id='" + task.id + "'" +
       checkedStatus +
       '><label>' +
-      task.title +
-      '</label></div></li>';
-    return liElement
+       task.title +
+       '</label></div></li>';
+
+    return liElement;
   }
 
   function toggleTask(e) {
     var itemId = $(e.target).data("id");
     var doneValue = Boolean($(e.target).is(':checked'));
-
     $.post("/tasks/" + itemId, {
       _method: "PUT",
       task: {
         done: doneValue
       }
-    });
+    }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
+    } );
   }
 
   $.get("/tasks").success( function( data ) {
