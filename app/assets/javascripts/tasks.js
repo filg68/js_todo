ddfd0bb0c4d6619cbf3e9b6d70665b50635804b7
Fil <runfilrun@gmail.com>
2019-04-25 18:47:43 +0000

    Applied css and added author section.

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -52,7 +52,7 @@ $(function() {
       var ulTodos = $('.todo-list');
       ulTodos.append(htmlString);
       $('.toggle').change(toggleTask);
-      
+      $('.new-todo').val('');
     });
   });
 });
